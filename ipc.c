049b312e71bac55f097c1d030d85214830de708e
Chilliou <Chilliou07@gmail.com>
2022-12-12 17:17:09 +0000

    V3

--- ipc.c
+++ ipc.c
@@ -1,3 +1,12 @@
+//
+// Created by ld201024 on 22/10/22.
+//
+/* ipc.c */
+/*
+   Ce programme vérifie l'équilibrage de la fonction rand 
+   https://github.com/Chilliou/ipc
+*/
+
 #include <stdio.h>          /* printf()                 */
 #include <stdlib.h>         /* exit(), malloc(), free() */
 #include <sys/types.h>      /* key_t, sem_t, pid_t      */
@@ -6,75 +15,97 @@
 #include <semaphore.h>      /* sem_open(), sem_destroy(), sem_wait().. */
 #include <fcntl.h>          /* O_CREAT, O_EXEC          */
 #include <time.h>
+#include <string.h>
 
 
 
-#define NB_FORK 10
+#define NB_FORK 2
 #define TABSIZE 1000000
-#define NB_RELANCE 10
-#define RAND_PAR_RELANCE 1000000000  
+#define RAND_PAR_RELANCE 1000000000  //Ne pas prendre une trop grosse valeur sinon ça bug
+
+key_t shmkey;                 
+int shmid;                   
+sem_t *sem;                   
+pid_t pid;                    
+struct donnees *commun;     
 
 
 struct donnees {
     int tabAlea[TABSIZE];
 };
 
+void generateChilds()
+{
+    // Génération des processus fils
+    for(int i = 0; i < NB_FORK; ++i ) {
+        // Si on est dans un processus fils
+        if( !fork() ) 
+        {
+            pid_t fid = getpid();         //Surement inutile mais sans le redéfinir j'avais 0
+            printf("%d : Start \n",fid);
 
-int main (int argc, char **argv){
-    key_t shmkey;                 
-    int shmid;                   
-    sem_t *sem;                   
-    pid_t pid;                    
-    struct donnees *commun;                      
-    srand((int)time(NULL));
-    int reponse;
+            srand(getpid());
 
-    shmkey = ftok(getenv("HOME"), 'A');     
+            int *array_n;
+            array_n = malloc((size_t)TABSIZE * sizeof(int));
 
-    shmid = shmget (shmkey,sizeof(struct donnees), 0644 | IPC_CREAT);
-    if (shmid < 0){                           
-        perror ("shmget\n");
-        exit (1);
+            if(!array_n)
+            {
+                fputs("SegFault\n",stderr);
+                exit(EXIT_FAILURE);
+            }
+
+            printf("%d : Start Rand Number \n",fid);
+
+            for( int i = 0; i < RAND_PAR_RELANCE; ++i )
+            {
+                array_n[rand() / (RAND_MAX / TABSIZE)]++;
+            }
+            printf("%d : End Rand Number:  \n",fid);
+
+            sem_wait (sem);  //Verrouiller un sémaphore 
+
+            printf("%d : Start Tab Filling and Mutex Lock Number\n",fid);
+
+            for( int i = 0; i < RAND_PAR_RELANCE; ++i )
+            {
+                commun->tabAlea[i] += array_n[i];
+            }
+
+
+            sem_post (sem); //Déverrouiller un sémaphore  
+
+
+            printf("%d : End Tab Filling and Mutex UnLock Number\n",fid);
+
+            free(array_n);      
+            
+           
+            printf("%d : End \n",fid);
+
+            exit (EXIT_SUCCESS);
+            
+        }
     }
 
-    commun = (struct donnees *) shmat (shmid, NULL, 0);   
-    sem = sem_open ("pSem", O_CREAT | O_EXCL, 0644, 25); //valeur du semmaphore je sais pas a quoi ça sert
+}
 
-    for (int i = 0; i < NB_FORK; i++)
-    {
-        pid = fork ();
-        if (pid < 0)  // Si il y a une erreur
-        { 
-            sem_unlink ("pSem");   
-            sem_close(sem);   
-            printf ("Fork error.\n");
-        }
-        else if (pid == 0)
-            break;             
-    }
+void caculateStat()
+{
+    //double moy_nbTab = RAND_PAR_RELANCE*NB_FORK/(sizeof(commun->tabAlea)/sizeof(commun->tabAlea[0]));
 
+    double moy_nbTab = 2000;  // Calculer à la main puisque sinon on overflow avec le calcul au dessus 
 
-    /******************************************************/
-    /******************   PARENT           ****************/
-    /******************************************************/
-    if (pid != 0){
-        /* wait for all children to exit */
-        while (pid = waitpid (-1, NULL, 0)){
-            if (errno == ECHILD)
-                break;
-        }
-
-
-    double moy_nbTab = 10000;  // Calculer à la main puisque sinon on overflow avec le calcul au dessus 
     double maxEqui = 0.0;
+    printf("tab contient %d\n", commun->tabAlea[5412]);
 
     for(int i=0;i<TABSIZE;i++)
     {
         double temp;
         double tabI = commun->tabAlea[i];
 
-        if(tabI<moy_nbTab)
-        {
+        if(tabI<moy_nbTab)  //Sert a verifier si on aurait pas une valeur trop basse 
+        {                   // Le pourcentage ce calcul dans les deux sens
             temp = (100.0*(moy_nbTab-tabI))/(moy_nbTab+100.0);
         }
         else
@@ -85,68 +116,47 @@ int main (int argc, char **argv){
     }
     maxEqui -=100;
     printf("Balancing of function rand is : %lf % \n", maxEqui);
+}
 
+
+int main (){
+                     
+    srand((int)time(NULL));
+
+    shmkey = ftok(getenv("HOME"), 'A');     //Convertir un nom de fichier et un identificateur de projet en clé IPC System V  
+
+    shmid = shmget (shmkey,sizeof(struct donnees), 0644 | IPC_CREAT);  // Allouer un segment de mémoire partagée  
+    if (shmid < 0)
+    {                           
+        perror ("shmget\n");
+        exit (1);
+    }
+
+    commun = (struct donnees *) shmat (shmid, NULL, 0);  //attache le segment de mémoire partagée
+    sem = sem_open ("pSem", O_CREAT | O_EXCL, 0644, 25); //Initialiser et ouvrir un sémaphore nommé  
+                                                         //O_CREAT est utlisé donc mode et value sont ignorés
+        
+
+
+    generateChilds();
+
+    while (pid = waitpid (-1, NULL, 0)){
+            if (errno == ECHILD)
+                break;
+        }
+
+
+    caculateStat();
+
+    /* shared memory detach */
     shmdt (commun);
     shmctl (shmid, IPC_RMID, 0);
 
+    /* cleanup semaphores */
     sem_unlink ("pSem");   
-    sem_close(sem); 
-
-    exit (EXIT_SUCCESS);
-}
-
-    /******************************************************/
-    /******************   CHILD           *****************/
-    /******************************************************/
-    else{
-        pid_t fid = getpid();
-        printf("%d : Start \n",fid);
-
-        for(int z=1;z<=NB_RELANCE;z++)
-        {
-            int *array_n;
-            array_n = malloc((size_t)RAND_PAR_RELANCE * sizeof(int));
-
-            if(!array_n)
-            {
-                fputs("SegFault\n",stderr);
-                return 1;
-            }
-
-            printf("%d : Start Rand Number: %d \n",fid,z);
-
-            int i=0;
-            while(i<RAND_PAR_RELANCE) 
-            {
-
-                array_n[i] = rand();
-                i++;
-            }
-            printf("%d : End Rand Number: %d \n",fid,z);
-
-            sem_wait (sem);
-
-            printf("%d : Start Tab Filling and Mutex Lock Number: %d \n",fid,z);
-
-            i=0;
-            while(i<RAND_PAR_RELANCE) 
-            {
-                
-                double produit = (double)array_n[i]/(double)RAND_MAX;
-                commun->tabAlea[(int)(produit*TABSIZE)]++; 
-                i++;
-            }
-
-            sem_post (sem);
-
-            printf("%d : End Tab Filling and Mutex UnLock Number: %d \n",fid,z);
-
-            free(array_n);  
-
-        }
-           
-        printf("%d : End \n",fid);
-
-        exit (EXIT_SUCCESS);
-    }
-}
+    sem_close(sem);  
+    
+    
+    exit(EXIT_SUCCESS);
+        
+}   
